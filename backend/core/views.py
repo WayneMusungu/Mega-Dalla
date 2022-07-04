6e8667925f4602c0ace7e1f6c14dea0ca88476e6
JamesMusembi <james.musembi@student.moringaschool.com>
2022-07-04 15:32:03 +0300

    editing forms

--- backend/core/views.py
+++ backend/core/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.views import View
 from django.views.generic import DetailView
 from .models import Item, OrderItem, Order, Address, Payment, Coupon, Refund, UserProfile
-from .forms import CheckoutForm, CouponForm, RefundForm, PaymentForm
+from .forms import  CouponForm, RefundForm, PaymentForm
 from core.forms import UserProfileForm
 
 
