d13fc7314ae088ad77cfa347fc4f3fc57b227043
JamesMusembi <james.musembi@student.moringaschool.com>
2022-07-04 15:24:47 +0300

    editing forms

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from . import views
 from django_registration.backends.one_step.views import RegistrationView
-from .views import ItemDetailView
+from .views import ItemDetailView, PaymentView
 
 
 urlpatterns=[
@@ -12,7 +12,7 @@ urlpatterns=[
     path('remove-from-cart/<slug>/', views.remove_from_cart, name="remove-from-cart"),
     path('profile-update/',views.update_profile, name='update_profile'),
     # path('accounts/', include('Account.urls')),
-    # path('payment/<payment_option>/', PaymentView.as_view(), name='payment'),
+    path('payment/<payment_option>/', PaymentView.as_view(), name='payment'),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include(('django.contrib.auth.urls', 'core'), namespace='login')),
     # path('accounts/', include('django.contrib.auth.urls')),
