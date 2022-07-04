6f3d7c8139bde5067eb5971f9856b2b189cabcdd
haimy <haimanaare@gmail.com>
2022-07-04 11:00:50 +0300

    installed all the requirements

--- backend/ecommerce/settings.py
+++ backend/ecommerce/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap5',
+    # 'bootstrap5',
     'crispy_forms',
     'cloudinary',
     'django_countries',
