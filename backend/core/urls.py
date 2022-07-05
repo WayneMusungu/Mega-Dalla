80e845250966086e7b2ae4c0c76003711543e256
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-07-05 11:13:27 +0300

    commits

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,26 +1,8 @@
+
 from django.urls import path, include
 from . import views
 from django_registration.backends.one_step.views import RegistrationView
 from .views import ItemDetailView,CheckoutView
-
-
-urlpatterns=[
-    path('',views.welcome, name='welcome'),
-    path('home/',views.home, name='home'),
-    path('checkout/', CheckoutView.as_view(), name='checkout'),
-    path('product/<slug>/', ItemDetailView.as_view(), name="product"),
-    path('add-to-cart/<slug>/', views.add_to_cart, name="add-to-cart"),
-    path('remove-from-cart/<slug>/', views.remove_from_cart, name="remove-from-cart"),
-    path('profile-update/',views.update_profile, name='update_profile'),
-    # path('accounts/', include('Account.urls')),
-    # path('accounts/', include('django_registration.backends.one_step.urls')),
-    # path('accounts/', include(('django.contrib.auth.urls', 'core'), namespace='login')),
-    # path('accounts/', include('django.contrib.auth.urls')),
-    # path('logout/', views.logout_user, name='logout'),
-]
-from django.urls import path, include
-from . import views
-from django_registration.backends.one_step.views import RegistrationView
 from .views import ItemDetailView, OrderSummaryView, remove_single_item_cart
 
 
@@ -28,6 +10,8 @@ urlpatterns=[
     path('',views.welcome, name='welcome'),
     path('home/',views.home, name='home'),
     path('product/<slug>/', ItemDetailView.as_view(), name="product"),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
+
     path('add-to-cart/<slug>/', views.add_to_cart, name="add-to-cart"),
     path('remove-from-cart/<slug>/', views.remove_from_cart, name="remove-from-cart"),
     path('remove-single-item-cart/<slug>',remove_single_item_cart, name='remove-single-item-cart'),
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,154 +1,6 @@
 from django.shortcuts import render
 from django.utils import timezone
 from django.contrib import messages
-from django.shortcuts import get_object_or_404,render,redirect
-from django.http import HttpResponse
-from django.core.exceptions import ObjectDoesNotExist
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login, logout
-from django.views.generic import DetailView,View
-from .models import Item, OrderItem, Order, Address, Payment, Coupon, Refund, UserProfile
-from core.forms import UserProfileForm,CheckoutForm,CouponForm
-
-# Create your views here.
-# def welcome(request):
-#     return HttpResponse('Welcome to the login page')
-def welcome(request):
-
-    return render(request, 'welcome.html')
-
-
-@login_required
-def home(request):
-    items = Item.objects.all()
-    print(items)
-    context = {
-        'items': items
-    }
-    return render(request, 'home.html', context)
-
-class ItemDetailView(DetailView):
-    model = Item
-    template_name = "product.html"
-
-
-def add_to_cart(request, slug):
-    item = get_object_or_404(Item, slug=slug)
-    order_item, created = OrderItem.objects.get_or_create(
-        item=item,
-        user=request.user,
-        ordered=False
-    )
-    order_qs = Order.objects.filter(user=request.user, ordered=False)
-
-    if order_qs.exists():
-        order = order_qs[0]
-        # Check if item is in cart
-        if order.items.filter(item__slug=item.slug).exists():
-            order_item.quantity += 1
-            order_item.save()
-            messages.success(request, "Item quantity was updated")
-
-        else:
-            messages.success(request, "Item was add to your cart")
-            order.items.add(order_item)
-
-    else:
-        ordered_date = timezone.now()
-        order = Order.objects.create(
-            user=request.user, ordered_date=ordered_date)
-        messages.success(request, "This item was add to your cart")
-
-        order.items.add(order_item)
-
-    return redirect("core:product", slug=slug)
-
-def remove_from_cart(request, slug):
-    item = get_object_or_404(Item, slug=slug)
-    order_qs = Order.objects.filter(
-        user=request.user,
-        ordered=False
-    )
-
-    if order_qs.exists():
-        order = order_qs[0]
-        print('order', order)
-        # Check if item is in cart
-        if order.items.filter(item__slug=item.slug).exists():
-            order_item = OrderItem.objects.filter(
-                item=item,
-                user=request.user,
-                ordered=False
-            )[0]
-            order.items.remove(order_item)
-            messages.success(request, "This item was removed from your cart")
-            return redirect("core:product", slug=slug)
-        else:
-            messages.success(request, "This item is not in your cart")
-            return redirect("core:product", slug=slug)
-    else:
-        # display message that order doesnt exist
-        messages.danger(request, "Item doesnt exist")
-        return redirect("core:product", slug=slug)
-
-
-
-class CheckoutView(View):
-    def get(self, *args, **kwargs):
-        try:
-            order = Order.objects.get(user=self.request.user, ordered=False)
-            form = CheckoutForm()
-            context = {
-                'form': form,
-                'couponform': CouponForm(),
-                'order': order,
-                'DISPLAY_COUPON_FORM': True
-            }
-
-            shipping_address_qs = Address.objects.filter(
-                user=self.request.user,
-                address_type='S',
-                default=True
-            )
-            if shipping_address_qs.exists():
-                context.update(
-                    {'default_shipping_address': shipping_address_qs[0]})
-
-            billing_address_qs = Address.objects.filter(
-                user=self.request.user,
-                address_type='B',
-                default=True
-            )
-            if billing_address_qs.exists():
-                context.update(
-                    {'default_billing_address': billing_address_qs[0]})
-            return render(self.request, "checkout.html", context)
-        except ObjectDoesNotExist:
-            messages.info(self.request, "You do not have an active order")
-            return redirect("core:checkout")
-
-
-@login_required(login_url='/accounts/login/')
-def update_profile(request):
-    current_user = request.user
-    form = UserProfileForm(request.POST, request.FILES)
-    if request.method == 'POST':
-        if form.is_valid():
-            details = form.save(commit=False)
-            details.user = request.user
-            details.save()
-            return redirect ('home')
-        else:
-            form = UserProfileForm()
-    return render(request, 'profile-update.html', {'form': form})
-
-@login_required
-def logout_user(request):
-
-    return render(request,'welcome.html')
-from django.shortcuts import render
-from django.utils import timezone
-from django.contrib import messages
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import get_object_or_404,render,redirect
 from django.http import HttpResponse
@@ -158,6 +10,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.views.generic import DetailView,View 
 from .models import Item, OrderItem, Order, Address, Payment, Coupon, Refund, UserProfile
 from core.forms import UserProfileForm
+from django.core.exceptions import ObjectDoesNotExist
+from core.forms import UserProfileForm,CheckoutForm,CouponForm
+
 
 # Create your views here.
 # def welcome(request):
@@ -241,6 +96,40 @@ def remove_from_cart(request, slug):
         # display message that order doesnt exist
         messages.danger(request, "Item doesnt exist")
         return redirect("core:product", slug=slug)
+    
+class CheckoutView(View):
+    def get(self, *args, **kwargs):
+        try:
+            order = Order.objects.get(user=self.request.user, ordered=False)
+            form = CheckoutForm()
+            context = {
+                'form': form,
+                'couponform': CouponForm(),
+                'order': order,
+                'DISPLAY_COUPON_FORM': True
+            }
+
+            shipping_address_qs = Address.objects.filter(
+                user=self.request.user,
+                address_type='S',
+                default=True
+            )
+            if shipping_address_qs.exists():
+                context.update(
+                    {'default_shipping_address': shipping_address_qs[0]})
+
+            billing_address_qs = Address.objects.filter(
+                user=self.request.user,
+                address_type='B',
+                default=True
+            )
+            if billing_address_qs.exists():
+                context.update(
+                    {'default_billing_address': billing_address_qs[0]})
+            return render(self.request, "checkout.html", context)
+        except ObjectDoesNotExist:
+            messages.info(self.request, "You do not have an active order")
+            return redirect("core:checkout")
 
 @login_required(login_url='/accounts/login/')
 def update_profile(request):
@@ -313,4 +202,5 @@ def remove_single_item_cart(request, slug):
         # display message that order doesnt exist
         messages.danger(request, "Item doesnt exist")
         return redirect("core:product", slug=slug)
+
     
