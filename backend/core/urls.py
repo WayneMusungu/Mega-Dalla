59f0c4a105000e3c530f50c344c51f02ffad184e
haimy <haimanaare@gmail.com>
2022-07-05 16:00:20 +0300

    styling

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from . import views
 from django_registration.backends.one_step.views import RegistrationView
-from .views import ItemDetailView, OrderSummaryView, remove_single_item_cart
+from .views import ItemDetailView, OrderSummaryView, remove_single_item_cart,update_profile
 
 
 urlpatterns=[
@@ -11,7 +11,7 @@ urlpatterns=[
     path('add-to-cart/<slug>/', views.add_to_cart, name="add-to-cart"),
     path('remove-from-cart/<slug>/', views.remove_from_cart, name="remove-from-cart"),
     path('remove-single-item-cart/<slug>',remove_single_item_cart, name='remove-single-item-cart'),
-    path('profile-update/',views.update_profile, name='update_profile'),
+    path('profile-update/',update_profile, name='update_profile'),
     path('order-summery/',OrderSummaryView.as_view(), name='order-summery'),
 
     # path('accounts/', include('Account.urls')),
