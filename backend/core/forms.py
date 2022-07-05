a7cd61deefe1b7d58060469447a245706fd91bd0
JamesMusembi <james.musembi@student.moringaschool.com>
2022-07-05 12:48:22 +0300

    fixing conflicts

--- backend/core/forms.py
+++ backend/core/forms.py
@@ -52,12 +52,4 @@ class CouponForm(forms.Form):
         'aria-label': 'Recipient\'s username',
         'aria-describedby': 'basic-addon2'
     }))
-from dataclasses import field
-from django.contrib.auth.models import User
-from django import forms
-from . models import UserProfile
 
-class UserProfileForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = ['bio','phone_number', 'fax_number']
