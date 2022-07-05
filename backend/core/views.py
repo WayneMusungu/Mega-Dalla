d937959316f35e54527e43483f111e26243622c4
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-07-05 22:29:44 +0300

    created login, signup and logout functions.

--- backend/core/views.py
+++ backend/core/views.py
@@ -6,8 +6,15 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.views.generic import DetailView
-from .models import Item, OrderItem, Order, Address, Payment, Coupon, Refund, UserProfile
-from core.forms import UserProfileForm
+from .models import Item, OrderItem, Order, Address, Payment, Coupon, Refund, User, UserProfile
+from core.forms import UserProfileForm,RegistrationForm,LoginForm
+
+from rest_framework import generics, viewsets
+from .serializers import AddressSerializer, CouponSerializer, ItemSerializer, OrderItemSerializer, OrderSerializer, PaymentSerializer, RefundSerializer, UserSerializer, UserProfileSerializer, LoginSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework import permissions, status
 
 # Create your views here.
 # def welcome(request):
@@ -104,7 +111,38 @@ def update_profile(request):
             form = UserProfileForm()
     return render(request, 'profile-update.html', {'form': form})
 
-@login_required
-def logout_user(request):
+def register(request):
+    form = RegistrationForm
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user =  form.save()
+            user.refresh_from_db()
+            user.profile.email = form.cleaned_data.get('email')
+            user.save()
+            form.save()
+        return redirect(login_user)
+    return render(request, 'auth/register.html',locals())
 
-    return render(request,'welcome.html')
+def login_user(request):
+    form=LoginForm()
+    if request.method=='POST':
+        form=LoginForm(request.POST)
+        if form.is_valid():
+            usern=form.cleaned_data['email']
+            passw=form.cleaned_data['password']
+            user=authenticate(request,username=usern,password=passw)
+            if user is not None:
+                login(request,user)
+                return redirect('core:home')
+            else:
+                return HttpResponse('Such a user does not exist')
+        else:
+            return HttpResponse("Form is not Valid")
+    
+    return render(request,'auth/login.html',locals())
+
+@login_required(login_url='/login')
+def logout_user(request):
+    logout(request)
+    return redirect('/')
