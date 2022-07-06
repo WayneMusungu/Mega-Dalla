0de7912854d1722ea9a7848ad03ba01ce07161d4
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-07-07 00:51:53 +0300

    added the vendor.

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,30 +1,5 @@
 from django.contrib import admin
-from .models import Item, OrderItem, Order, Payment, Coupon, Refund, Address, User, UserProfile
-from django.contrib.auth.admin import UserAdmin as BaseUser
-from django.utils.translation import gettext as _
-
-# from core import models
-
-# Register your models here.
-
-
-class UserAdmin(BaseUser):
-    ordering = ['id']
-    list_display=   ['id', 'username', 'email', 'name']
-    list_display_links=['id','email']
-    fieldsets= (
-        (None, {'fields': ('username', 'email','password',)}),
-        (_('Personal info'), {'fields': ('name',)}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',)}),
-        (_('Imp dates'), {'fields': ('last_login',)})   
-    )
-    add_fieldsets= (
-        (None, {
-            'class': ('wide'),
-            'fields': ('name', 'email', 'password1','password2',)
-        }),
-    )
-    
+from .models import Item, OrderItem, Order, Payment, Coupon, Refund, Address, User, UserProfile, Vendor
 
 
 def make_refund_accepted(modeladmin, request, queryset):
@@ -88,4 +63,5 @@ admin.site.register(Coupon)
 admin.site.register(Refund)
 admin.site.register(Address, AddressAdmin)
 admin.site.register(UserProfile)
-admin.site.register(User,UserAdmin)
+admin.site.register(User)
+admin.site.register(Vendor)
