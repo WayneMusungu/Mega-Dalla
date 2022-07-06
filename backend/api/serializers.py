d505e47a12ac5ef5e84c60fe035fbc282939b8ae
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-07-06 23:42:22 +0300

    updatec the serializers.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,43 +1,27 @@
 from rest_framework import serializers
 from .models import *
-from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth import get_user_model
 from core.models import *
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields =('email','username','password', 'is_active','is_staff')
-        extra_kwargs = {'password':{'write_only':True,'min_length':8}}
+        fields =('username','email', 'is_customer','is_vendor','password')
+        extra_kwargs = {'password':{'write_only':True,'min_length':6}}
 
     def create(self,validated_data):
         return get_user_model().objects.create_user(**validated_data)
-
-class LoginSerializer(serializers.Serializer):
-    email = serializers.CharField()
-    password = serializers.CharField(
-        style={'input_type':'password'},
-        trim_whitespace = False
-    )
-    def validate(self,attrs):
-        email = attrs.get('email')
-        password = attrs.get('password')
-
-        user = authenticate(
-            request=self.context.get('request'),
-            email=email,
-            password=password
-        )
-        if not user:
-            raise serializers.ValidationError("Invalid User Credentials")
-        attrs['user'] =user
-        return attrs
-
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = ['user','stripe_customer_id','phone_number','bio',]
+        
+class VendorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Vendor
+        fields ='__all__'
         
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
