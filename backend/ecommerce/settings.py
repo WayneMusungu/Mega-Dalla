889cd502c646ec744e1ae8d25f158ffab4a1ea02
JamesMusembi <james.musembi@student.moringaschool.com>
2022-07-06 18:45:42 +0300

    styling

--- backend/ecommerce/settings.py
+++ backend/ecommerce/settings.py
@@ -143,7 +143,15 @@ USE_TZ = True
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
