5bd02df79ee8009915d1396a5d578499bc21e921
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-07-07 15:25:40 +0300

    styles

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,11 +2,7 @@
 from django.urls import path, include
 from . import views
 from django_registration.backends.one_step.views import RegistrationView
-<<<<<<< HEAD
-from .views import ItemDetailView, OrderSummaryView, remove_single_item_cart,update_profile, CheckoutView
-=======
 from .views import CheckoutView, ItemDetailView, OrderSummaryView, remove_single_item_cart,update_profile
->>>>>>> a60129a9cc93f7bcfdade7a65a69f17b7a29f30b
 
 
 urlpatterns=[
