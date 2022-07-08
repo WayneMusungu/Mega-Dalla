61fcdeb554e5779e3109bfb264e61d06caad3c27
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-07-08 21:50:55 +0300

    created components and models.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { AuthComponent } from './auth/auth.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -11,13 +11,27 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthService } from './services/auth.service';
 import { ProfileComponent } from './profile/profile.component';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { HomeComponent } from './components/home/home.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { PaymentComponent } from './components/payment/payment.component';
+import { OrdersummaryComponent } from './components/ordersummary/ordersummary.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProfileUpdateComponent } from './components/profile-update/profile-update.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     AuthComponent,
-    ProfileComponent
+    ProfileComponent,
+    HomeComponent,
+    CheckoutComponent,
+    PaymentComponent,
+    OrdersummaryComponent,
+    ProductComponent,
+    ProfileUpdateComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,30 +1,30 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { AuthService } from '../services/auth.service';
-import { User } from '../models/auth';
+// import { Component, OnDestroy, OnInit } from '@angular/core';
+// import { Subscription } from 'rxjs';
+// import { AuthService } from '../services/auth.service';
+// import { User } from '../models/auth';
 
-@Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
-})
-export class NavbarComponent implements OnInit {
+// @Component({
+//   selector: 'app-navbar',
+//   templateUrl: './navbar.component.html',
+//   styleUrls: ['./navbar.component.css']
+// })
+// export class NavbarComponent implements OnInit {
 
-  isAuthenticated:boolean = false;
-  private userSub: Subscription;
+//   isAuthenticated:boolean = false;
+//   private userSub: Subscription;
 
-  constructor(public authService: AuthService) { }
+//   constructor(public authService: AuthService) { }
 
-  ngOnInit() {
-    this.userSub= this.authService.user.subscribe((user) =>{
-      this.isAuthenticated=!user? false : true;
-    })
-  }
-  ngOnDestroy() {
-      this.userSub.unsubscribe();
-  }
-  onLogout(){
-    this.authService.logout();
-  }
+//   ngOnInit() {
+//     this.userSub= this.authService.user.subscribe((user) =>{
+//       this.isAuthenticated=!user? false : true;
+//     })
+//   }
+//   ngOnDestroy() {
+//       this.userSub.unsubscribe();
+//   }
+//   onLogout(){
+//     this.authService.logout();
+//   }
 
-}
+// }
