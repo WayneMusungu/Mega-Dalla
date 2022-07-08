91af398684c1188ffcd92bd845f8f89f77258b50
JamesMusembi <james.musembi@student.moringaschool.com>
2022-07-08 15:38:32 +0300

    fixing conflicts

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-
-from django.urls import path, include
-from . import views
-from django.conf import settings 
-from django.conf.urls.static import static
-from django_registration.backends.one_step.views import RegistrationView
-from .views import CheckoutView, ItemDetailView, OrderSummaryView, remove_single_item_cart,update_profile
-
-
-urlpatterns=[
-    path('',views.welcome, name='welcome'),
-    path('home/',views.home, name='home'),
-    path('product/<slug>/', ItemDetailView.as_view(), name="product"),
-    path('add-to-cart/<slug>/', views.add_to_cart, name="add-to-cart"),
-    path('remove-from-cart/<slug>/', views.remove_from_cart, name="remove-from-cart"),
-    path('remove-single-item-cart/<slug>',remove_single_item_cart, name='remove-single-item-cart'),
-    path('profile-update/',update_profile, name='update_profile'),
-    path('order-summery/',OrderSummaryView.as_view(), name='order-summery'),
-    path('checkout/', CheckoutView.as_view(), name='checkout'),
-    # path('payment/<payment_option>/', PaymentView.as_view(), name='payment'),
-
-
-    # path('accounts/', include('Account.urls')),
-    path('accounts/', include('django_registration.backends.one_step.urls')),
-    path('accounts/', include(('django.contrib.auth.urls', 'core'), namespace='login')),
-    # path('accounts/', include('django.contrib.auth.urls')),
-    path('logout/', views.logout_user, name='logout'),
-
-]
-
-
-if settings.DEBUG:
-    urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-=======
 
 from django.urls import path, include
 from . import views
@@ -62,4 +27,3 @@ urlpatterns=[
     path('logout/', views.logout_user, name='logout'),
 
 ]
->>>>>>> e029b4c53a52413c2babd31c4136b45f9ce5d12f
