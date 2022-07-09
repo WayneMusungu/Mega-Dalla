67e78387eede6aeda05525d9e9355c83911723cb
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-07-09 18:53:15 +0300

    created a function for product items api.

--- frontend/src/app/components/product/product.component.ts
+++ frontend/src/app/components/product/product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  item!: Item;
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+
+    this.productService.GetData().subscribe((item) => {
+      console.log(item);
+      this.item = item;
+    })
   }
 
 }
