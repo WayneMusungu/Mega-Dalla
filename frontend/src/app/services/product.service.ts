4d57d01b9402e9276c2edc8c245ecf729b827e34
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-07-09 18:47:55 +0300

    function to getdata from our api.

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Item } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private url = `${environment.apiUrl}`;
+  
+
+  constructor(private http: HttpClient) { }
+
+  GetData(): Observable<Item> {
+    return this.http.get<Item>(`${this.url}/items`);
+  }
+
 }
