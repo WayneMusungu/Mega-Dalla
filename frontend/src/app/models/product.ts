a406d7d9b218285ddfb8695cec5b298391cd5652
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-07-09 18:50:45 +0300

    created the model for the items.

---
+++ frontend/src/app/models/product.ts
@@ -0,0 +1,10 @@
+export interface Item{
+    title: string;
+    price: number;
+    discount_price: number;
+    category: string;
+    label: string;
+    slug: string;
+    description: string;
+    image: string;
+}
