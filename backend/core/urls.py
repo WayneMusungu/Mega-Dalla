475ea4bf6620ea3010cfc2c24ffdc09fe1751e8c
Clinton wambugu <clintonwambugu@gmail.com>
2022-07-10 19:33:16 +0300

    Merge changes to app

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-
-from django.urls import path, include
-from . import views
-from django_registration.backends.one_step.views import RegistrationView
-from .views import (
-    ItemDetailView,
-    OrderSummaryView,
-    remove_single_item_cart,
-    update_profile,
-    CheckoutView
-)
-
-
-urlpatterns=[
-    path('',views.welcome, name='welcome'),
-    path('home/',views.home, name='home'),
-    path('product/<slug>/', ItemDetailView.as_view(), name="product"),
-    path('add-to-cart/<slug>/', views.add_to_cart, name="add-to-cart"),
-    path('remove-from-cart/<slug>/', views.remove_from_cart, name="remove-from-cart"),
-    path('remove-single-item-cart/<slug>',remove_single_item_cart, name='remove-single-item-cart'),
-    path('profile-update/',update_profile, name='update_profile'),
-    path('order-summery/',OrderSummaryView.as_view(), name='order-summery'),
-    path('checkout/', CheckoutView.as_view(), name='checkout'),
-
-    # path('accounts/', include('Account.urls')),
-    path('accounts/', include('django_registration.backends.one_step.urls')),
-    path('accounts/', include(('django.contrib.auth.urls', 'core'), namespace='login')),
-    # path('accounts/', include('django.contrib.auth.urls')),
-    path('logout/', views.logout_user, name='logout'),
-
-]
-=======
 
 from django.urls import path, include
 from . import views
@@ -60,4 +27,3 @@ urlpatterns=[
     path('logout/', views.logout_user, name='logout'),
 
 ]
->>>>>>> f36b715f203a1deb2d197a678232fea17c575f4b
