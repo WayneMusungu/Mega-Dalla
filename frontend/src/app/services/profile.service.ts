0ff528a2f0f3bdbd947e101bcdc192749b62faee
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-07-11 20:31:49 +0300

    more minor updates.

--- frontend/src/app/services/profile.service.ts
+++ frontend/src/app/services/profile.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Profile } from '../models/profile';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  private url = `${environment.apiUrl}`;
+
+  constructor(private http: HttpClient) { }
+
+
+  upDate(profile: Profile ){
+    return this.http.put<Profile>(`${this.url}/profile/`,profile);
+    
+  }
 }
