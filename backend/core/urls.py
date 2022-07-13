8a3773d89089f3d6723201b06f52563da8478618
Clinton wambugu <clintonwambugu@gmail.com>
2022-07-13 21:24:06 +0300

    Style login page

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 from django.urls import path, include
 from . import views
 from django_registration.backends.one_step.views import RegistrationView
@@ -30,34 +28,3 @@ urlpatterns=[
     path('logout/', views.logout_user, name='logout'),
 
 ]
-=======
-
-from django.urls import path, include
-from . import views
-from django_registration.backends.one_step.views import RegistrationView
-from .views import CheckoutView, ItemDetailView, OrderSummaryView, remove_single_item_cart,update_profile
-
-
-urlpatterns=[
-    path('',views.welcome, name='welcome'),
-    path('home/',views.home, name='home'),
-    path('product/<slug>/', ItemDetailView.as_view(), name="product"),
-    path('add-to-cart/<slug>/', views.add_to_cart, name="add-to-cart"),
-    path('remove-from-cart/<slug>/', views.remove_from_cart, name="remove-from-cart"),
-    path('remove-single-item-cart/<slug>',remove_single_item_cart, name='remove-single-item-cart'),
-    path('profile-update/',update_profile, name='update_profile'),
-    path('order-summery/',OrderSummaryView.as_view(), name='order-summery'),
-    path('checkout/', CheckoutView.as_view(), name='checkout'),
-    # path('payment/<payment_option>/', PaymentView.as_view(), name='payment'),
-
-
-    # path('accounts/', include('Account.urls')),
-    # path('accounts/', include('django_registration.backends.one_step.urls')),
-    # path('accounts/', include(('django.contrib.auth.urls', 'core'), namespace='login')),
-    # path('accounts/', include('django.contrib.auth.urls')),
-    path('register/', views.register),
-    path('login/', views.login_user),
-    path('logout/', views.logout_user, name='logout'),
-
-]
->>>>>>> master
